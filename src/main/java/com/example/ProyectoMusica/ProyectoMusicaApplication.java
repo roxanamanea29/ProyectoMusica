bbb4a00c3d1963b59de23c57f30f940cff0ecef8
Roxana Manea <manea.roxanaa@gmail.com>
2024-04-26 09:10:55 +0200

    1 commmit

---
+++ src/main/java/com/example/ProyectoMusica/ProyectoMusicaApplication.java
@@ -0,0 +1,13 @@
+package com.example.ProyectoMusica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoMusicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoMusicaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ProyectoMusica/ProyectoMusicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ProyectoMusica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoMusicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
