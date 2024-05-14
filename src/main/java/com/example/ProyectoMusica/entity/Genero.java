e4063f83e9c0ab0859394a05a0c7d05b2fbdb006
gerar <gvaldezmad@gmail.com>
2024-05-14 11:24:13 +0200

    Genero añadido

---
+++ src/main/java/com/example/ProyectoMusica/entity/Genero.java
@@ -0,0 +1,27 @@
+package com.example.ProyectoMusica.entity;
+
+public class Genero {
+    private int idGenero;
+    private String nombreGenero;
+
+    public Genero(int idGenero, String nombreGenero) {
+        this.idGenero = idGenero;
+        this.nombreGenero = nombreGenero;
+    }
+
+    public int getIdGenero() {
+        return idGenero;
+    }
+
+    public void setIdGenero(int idGenero) {
+        this.idGenero = idGenero;
+    }
+
+    public String getNombreGenero() {
+        return nombreGenero;
+    }
+
+    public void setNombreGenero(String nombreGenero) {
+        this.nombreGenero = nombreGenero;
+    }
+}
